8d5c2fe52b2f0c7409b200bc27b668d212b39ec5
Zachariah Ndungu <ndunguzack@gmail.com>
2026-01-22 18:35:21 +0300

    data definition for users table. relationship with trades defined

--- backend/db/migrate.js
+++ backend/db/migrate.js
@@ -1,8 +1,16 @@
 import db from "./db.js";
 
-db.prepare(`
+db.exec(`
+  CREATE TABLE IF NOT EXISTS users (
+  id TEXT PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
+  password_hash TEXT NOT NULL,
+  created_at TEXT DEFAULT CURRENT_TIMESTAMP
+  );
+
   CREATE TABLE IF NOT EXISTS trades (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT NOT NULL,
     instrument TEXT NOT NULL,
     direction TEXT CHECK(direction IN ('long', 'short')) NOT NULL, 
     risk_reward REAL NOT NULL,
@@ -16,8 +24,9 @@ db.prepare(`
     strategy TEXT,
     pre_notes TEXT,
     post_notes TEXT,
-    created_at TEXT DEFAULT CURRENT_TIMESTAMP
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(user_id) REFERENCES users(id)
   );
-`).run();
+`);
 
 console.log("Database initialized");
