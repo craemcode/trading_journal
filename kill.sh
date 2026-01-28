bab81de2bf102a85724ed3ce325b6220d2fb35a3
Zachariah Ndungu <ndunguzack@gmail.com>
2026-01-28 17:57:12 +0300

    created scripts for starting the application

---
+++ kill.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+cd ~/trading_journal
+echo "Stopping Trade Journal..."
+
+docker compose down
+
+echo "Trade Journal stopped."
--- run_journal.sh
+++ run_journal.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "Starting Trade Journal..."
+echo "Welcome to my trade journal. Built by: Zacharia Github @craemcode"
+echo "--------starting-------------"
+
+cd ~/trading_journal
+
+echo "--------------Starting docker container-------------"
+
 
 docker compose up --build
